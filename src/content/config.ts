108ad0b0bdab157cf8da77420ae9aacd2db91585
Damien Stanton <damien.stanton@gmail.com>
2026-04-21 13:43:37 -0400

    feat: add content collection schema

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+
+const SECTIONS = ['About', 'Independent Study', 'MSCS'] as const;
+
+const articles = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    section: z.enum(SECTIONS),
+    date: z.date().optional(),
+    description: z.string().optional(),
+  }),
+});
+
+export const collections = { articles };
